a4126ca6a4fd855923a72c0308f6f1d0c43d1a81
Muhammad <muhannmmad@gmail.com>
2024-11-28 16:14:35 +0100

    all made just few decorations left and my own quot added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Zitat App',
       home: QuoteScreen(),
     );
@@ -17,13 +20,37 @@ class MyApp extends StatelessWidget {
 }
 
 class QuoteScreen extends StatefulWidget {
+  const QuoteScreen({super.key});
+
   @override
-  _QuoteScreenState createState() => _QuoteScreenState();
+  QuoteScreenState createState() => QuoteScreenState();
 }
 
-class _QuoteScreenState extends State<QuoteScreen> {
-  String quote = 'click here to get new quotation.';
+class QuoteScreenState extends State<QuoteScreen> {
+  String quote = '';
+
   String author = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadSavedQuote();
+  }
+
+  Future<void> loadSavedQuote() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      quote = prefs.getString('quote') ??
+          'As me in life as a feather in the air missed its way to its aim so it left itself to the wind to lead it anywhere hopeless and aimless ';
+      author = prefs.getString('author') ?? 'Muhammad Hassan';
+    });
+  }
+
+  Future<void> saveQuote(String quote, String author) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('quote', quote);
+    await prefs.setString('author', author);
+  }
 
   Future<void> fetchQuote() async {
     final url = Uri.parse('https://api.api-ninjas.com/v1/quotes');
@@ -40,6 +67,7 @@ class _QuoteScreenState extends State<QuoteScreen> {
         quote = data[0]['quote'];
         author = data[0]['author'];
       });
+      await saveQuote(quote, author);
     } else {
       setState(() {
         quote = 'Fehler beim Abrufen des Zitats.';
@@ -48,61 +76,94 @@ class _QuoteScreenState extends State<QuoteScreen> {
     }
   }
 
+  Future<void> deleteQuote() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('quote');
+    await prefs.remove('author');
+    setState(() {
+      quote = 'click to get a new Quotation';
+      author = '';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Positioned.fill(
-          child: Image.asset(
-            'assets/09c7b5584ff749eb34455a54ee657331.jpg',
-            fit: BoxFit.cover,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '"$quote"',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      color: const Color.fromARGB(255, 0, 83, 151)),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  author.isNotEmpty ? '- $author' : '',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: const Color.fromARGB(255, 123, 0, 0),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 20),
-              ],
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/09c7b5584ff749eb34455a54ee657331.jpg',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
-        Positioned(
-          bottom: 100,
-          right: 100,
-          left: 100,
-          child: ElevatedButton(
-            onPressed: fetchQuote,
-            child: Text(
-              'Get new quotation',
-              style: TextStyle(
-                fontSize: 18,
+          Positioned(
+            top: 100,
+            right: 20,
+            child: SizedBox(
+              width: 100,
+              height: 100,
+              child: Image.asset(
+                'assets/1294.png',
               ),
             ),
           ),
-        ),
-      ]),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '"$quote"',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Color.fromARGB(255, 0, 83, 151),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    author.isNotEmpty ? '- $author' : '',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Color.fromARGB(255, 123, 0, 0),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 180,
+            left: 100,
+            right: 100,
+            child: ElevatedButton(
+              onPressed: fetchQuote,
+              child: const Text(
+                'Get new Quotation',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 120,
+            left: 80,
+            right: 80,
+            child: ElevatedButton(
+              onPressed: deleteQuote,
+              child: const Text(
+                'Delete saved Quotation',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
